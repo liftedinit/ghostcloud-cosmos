7165bf5b4a0cbe1ed1ab9c133d0430de80339230
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-11-02 11:20:41 -0400

    feat: list filter and format

--- x/ghostcloud/keeper/query_meta.go
+++ x/ghostcloud/keeper/query_meta.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"strings"
 
 	"ghostcloud/x/ghostcloud/types"
 
@@ -11,6 +12,39 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+func metaPassesAllFilters(item *types.Meta, filters []*types.Filter) bool {
+	for _, filter := range filters {
+		if !metaPassesFilter(item, filter) {
+			return false
+		}
+	}
+	return true
+}
+
+func metaPassesFilter(item *types.Meta, filter *types.Filter) bool {
+	var itemValue string
+
+	switch filter.Field {
+	case types.Filter_CREATOR:
+		itemValue = item.Creator
+	default:
+		return false
+	}
+
+	switch filter.Operator {
+	case types.Filter_EQUAL:
+		return itemValue == filter.Value
+	case types.Filter_NOT_EQUAL:
+		return itemValue != filter.Value
+	case types.Filter_CONTAINS:
+		return strings.Contains(itemValue, filter.Value)
+	case types.Filter_NOT_CONTAINS:
+		return !strings.Contains(itemValue, filter.Value)
+	default:
+		return false
+	}
+}
+
 func (k Keeper) Metas(goCtx context.Context, req *types.QueryMetasRequest) (*types.QueryMetasResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
@@ -19,5 +53,17 @@ func (k Keeper) Metas(goCtx context.Context, req *types.QueryMetasRequest) (*typ
 
 	metas := k.GetAllMeta(ctx)
 
-	return &types.QueryMetasResponse{Meta: metas}, nil
+	var filteredMetas []*types.Meta
+	// Filter the result if necessary
+	if req.Filters != nil {
+		for _, meta := range metas {
+			if metaPassesAllFilters(meta, req.Filters) {
+				filteredMetas = append(filteredMetas, meta)
+			}
+		}
+	} else {
+		filteredMetas = metas
+	}
+
+	return &types.QueryMetasResponse{Meta: filteredMetas}, nil
 }
